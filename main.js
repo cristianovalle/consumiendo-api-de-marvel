796788ae43ec6dc7a6c6ea76249b88d61d3267cd
Diego Aguilar <daafaguilar98@gmail.com>
2018-10-09 13:32:13 -0500

    agregue commit

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 new Vue({
   el:'#app',
   data: {
-      heroes: []
+      heroes: [
+        {
+          state:false;
+        }
+        seleccion:0;
+      ]
   },
   mounted: function(){
     this.cargarpersonajes();
   },
   methods:{
     cargarpersonajes(){
-      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=5&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
+      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=10&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
       .then((respuesta)=>{
         this.heroes = respuesta.body.data.results
       });
+    }
+  },
+  methods: {
+    seleccionarherore(item){
+      if (item.state){
+          item.state=false;
+          this.seleccion-=item.seleccion;
+      }
+      else{
+        item.state=true;
+        this.seleccion += item.seleccion;
+      }
     }
   }
 });
