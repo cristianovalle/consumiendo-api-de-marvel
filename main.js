25355cbec4b46847d24d14718447f832a77f9d60
Diego Aguilar <daafaguilar98@gmail.com>
2018-10-11 11:51:07 -0500

    cambie el metodo

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ new Vue({
   el:'#app',
   data: {
       heroes: [],
+      state: false,
       seleccion:0
   },
   mounted: function(){
@@ -9,20 +10,20 @@ new Vue({
   },
   methods:{
     cargarpersonajes(){
-      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=12&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
+      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=14&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
       .then((respuesta)=>{
         this.heroes = respuesta.body.data.results
       });
     },
-    seleccionarherore(item){
-      if (item.state){
-          item.state=false;
-          this.seleccion-=item.seleccion;
+    seleccionarheroe(item){
+        if (item.state){
+            item.state=false;
+            this.seleccion-=item.thumbnail;
+        }
+        else{
+          item.state=true;
+          this.seleccion += item.thumbnail;
+        }
       }
-      else{
-        item.state=true;
-        this.seleccion += item.seleccion;
-      }
-    }
   }
 });
