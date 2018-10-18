a0dcae8f529c8dc2de2cb54db1b702c7c303230b
Diego Aguilar <daafaguilar98@gmail.com>
2018-10-18 11:56:03 -0500

    agrege boton

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ new Vue({
       heroes2:[],
       heroe: {},
       heroe2: {},
-      state: false,
-      seleccion:0
+      seleccion:0,
+      seleccion2:0
   },
   mounted: function(){
     this.cargarpersonajes();
@@ -15,27 +15,23 @@ new Vue({
   },
   methods:{
     cargarpersonajes(){
-      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=6&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
+      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=100&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
       .then((respuesta)=>{
         this.heroes=respuesta.body.data.results
         this.heroes2=respuesta.body.data.results.splice(respuesta.body.data.results.length/2,respuesta.body.data.results.length)
-
+        this.random()
       });
     },
     random(){
       this.heroe = this.heroes[Math.floor(Math.random()*this.heroes.length)];
       this.heroe2 = this.heroes2[Math.floor(Math.random()*this.heroes2.length)];
     },
-    seleccionarheroe(item){
-        if (item.state){
-            item.state=false;
-            this.seleccion-=item.thumbnail;
-        }
-        else{
-          item.state=true;
-          this.seleccion += item.thumbnail;
-        }
-      }      
+    seleccionarheroe1(heroes){
+        this.seleccion+=1;
+      },
+      seleccionarheroe2(heroes2){
+        this.seleccion2+=1;
+        }       
   },
 });
 
