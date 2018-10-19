221daf24ead12730a1635dd1ddbeb1c7ed57cb39
Diego Aguilar <daafaguilar98@gmail.com>
2018-10-19 11:30:22 -0500

    corregi la logica

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ new Vue({
   data: {
       heroes: [],
       heroes2:[],
-      heroe: {},
-      heroe2: {},
       seleccion:0,
-      seleccion2:0
+      seleccion2:0,
+      heroe: null,
+      heroe2: null
   },
   mounted: function(){
     this.cargarpersonajes();
-    this.heroe = this.heroes[Math.floor(Math.random()*this.heroes.length)];
-
   },
   methods:{
     cargarpersonajes(){
-      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=100&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
+      
+      this.$http.get('https://gateway.marvel.com/v1/public/characters?orderBy=name%2Cmodified&limit=10&apikey=602ea0bd37dc91bf34664b50f1e34d40&ts=1&hash=16688171fc500807f4b0f1a0cec969ae')
       .then((respuesta)=>{
         this.heroes=respuesta.body.data.results
         this.heroes2=respuesta.body.data.results.splice(respuesta.body.data.results.length/2,respuesta.body.data.results.length)
@@ -26,12 +25,13 @@ new Vue({
       this.heroe = this.heroes[Math.floor(Math.random()*this.heroes.length)];
       this.heroe2 = this.heroes2[Math.floor(Math.random()*this.heroes2.length)];
     },
-    seleccionarheroe1(heroes){
-        this.seleccion+=1;
-      },
-      seleccionarheroe2(heroes2){
-        this.seleccion2+=1;
-        }       
+    seleccionarheroe1(){
+        this.seleccion += 1;
+        this.random()
+    },
+    seleccionarheroe2(){
+        this.seleccion2 += 1;
+        this.random()
+    }       
   },
 });
-
